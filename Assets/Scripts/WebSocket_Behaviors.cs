4e165d40f4aa4500e42c1eee1276c4fe91390db5
Nelson Diaz <nadiaz2@wpi.edu>
2023-10-28 16:08:50 -0400

    Improve IP address lookup for QR code

--- Assets/Scripts/WebSocket_Behaviors.cs
+++ Assets/Scripts/WebSocket_Behaviors.cs
@@ -2,7 +2,7 @@ using System;
 using WebSocketSharp;
 using WebSocketSharp.Server;
 
-namespace My_Websocket {
+namespace WebSocketBehaviors {
     public class Echo : WebSocketBehavior
     {
         protected override void OnMessage (MessageEventArgs e)
