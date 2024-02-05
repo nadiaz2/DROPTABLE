38fa7e7588a3d3bd0cdb3f2cfeb3e58565399859
Botao Han <73296496+124135417@users.noreply.github.com>
2024-02-06 00:20:21 +0800

    Added progressive app compatibility

--- HTTP_Vue/src/main.js
+++ HTTP_Vue/src/main.js
@@ -4,10 +4,16 @@ import { createVuetify } from 'vuetify';
 import 'vuetify/styles';  // Ensure to import Vuetify styles
 import router from "./router";
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import { registerSW } from 'virtual:pwa-register';
 
 
 const app = createApp(App);
 
+const updateSW = registerSW({
+    onNeedRefresh() {},
+    onOfflineReady() {},
+  });
+  
 const vuetify = createVuetify(); // Initialize Vuetify
 
 app.use(vuetify);
--- HTTP_Vue/vite.config.js
+++ HTTP_Vue/vite.config.js
@@ -2,6 +2,7 @@
 import vue from '@vitejs/plugin-vue'
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 import ViteFonts from 'unplugin-fonts/vite'
+import { VitePWA } from 'vite-plugin-pwa' // Import the VitePWA plugin
 
 // Utilities
 import { defineConfig } from 'vite'
@@ -22,10 +23,35 @@ export default defineConfig({
     }),
     ViteFonts({
       google: {
-        families: [{
-          name: 'Roboto',
-          styles: 'wght@100;300;400;500;700;900',
-        }],
+      families: [{
+        name: 'Roboto',
+        styles: 'wght@100;300;400;500;700;900',
+      }],
+      },
+    }),
+    // Add the VitePWA plugin configuration here
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Your App Name',
+        short_name: 'App',
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
       },
     }),
   ],
