57bf559b2fbc4821f148e438cc2041dfdf45f5f0
Nelson Diaz <nadiaz2@wpi.edu>
2024-04-12 22:25:28 -0400

    Fix spline movement end

--- Assets/Scripts/AI/SplineMovement.cs
+++ Assets/Scripts/AI/SplineMovement.cs
@@ -39,20 +39,21 @@ public class SplineMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!animate || (distAmount >= splineLength))
+        if(!animate || (distAmount >= splineLength) || (distAmount <= 0.0f))
         {
             _moving = false;
             return;
         }
         _moving = true;
 
-        distAmount += Time.deltaTime * speed;
-        float percentPos = Mathf.Min(distAmount / splineLength, 1.0f);
+        float mult = reverseMovement ? -1.0f : 1.0f;
+        distAmount += mult * Time.deltaTime * speed;
+        float percentPos = Mathf.Clamp(distAmount / splineLength, 0.0f, 1.0f);
 
-        float usePercent = reverseMovement ? (1.0f-percentPos) : percentPos;
+        //float usePercent = reverseMovement ? (1.0f-percentPos) : percentPos;
 
-        float3 splinePosition = splineContainer.EvaluatePosition(splineIndex, usePercent);
-        float3 splineTangent = splineContainer.EvaluateTangent(splineIndex, usePercent);
+        float3 splinePosition = splineContainer.EvaluatePosition(splineIndex, percentPos);
+        float3 splineTangent = splineContainer.EvaluateTangent(splineIndex, percentPos);
 
         RaycastHit Hit;
         Physics.Raycast(splinePosition, -Vector3.up, out Hit, 500.0f, groundLayers);
@@ -60,7 +61,7 @@ public class SplineMovement : MonoBehaviour
         Vector3.ProjectOnPlane(new Vector3(), Vector3.up);
 
         transform.position = new Vector3(splinePosition.x, Hit.point.y, splinePosition.z);
-        transform.LookAt(transform.position + new Vector3(splineTangent.x, 0.0f, splineTangent.z));
+        transform.LookAt(transform.position + new Vector3(mult * splineTangent.x, 0.0f, mult * splineTangent.z));
     }
 
     public void StartMovement() {
