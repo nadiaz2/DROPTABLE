6dabd07043789af1be2a3e3a16c00754aedf230d
Bright183 <brightlin183@gmail.com>
2024-04-14 01:03:30 -0400

    Make Jacob follow player in clothing store

--- Assets/JacobFollow.cs
+++ Assets/JacobFollow.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,13 @@ public class JacobFollow : MonoBehaviour
     public Transform player;
     private NavMeshAgent agent;
 
+    [Header("Animator")]
+    public Animator animator;
+
+    [Header("NavMeshController")]
+    public int waitFrames;
+    private int stillFrames = 0;
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +27,28 @@ public class JacobFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        agent.destination = player.position;
+        agent.SetDestination(player.position);
+
+
+        if(agent.velocity.magnitude < 5f)
+        {
+            stillFrames = Math.Min(stillFrames+1, waitFrames);
+        }
+        else
+        {
+            stillFrames = 0;
+        }
+
+
+        if (stillFrames >= waitFrames)
+        {
+            animator.SetBool("IsWalking", false);
+            animator.SetBool("IsStanding", true);
+        }
+        else
+        {
+            animator.SetBool("IsWalking", true);
+            animator.SetBool("IsStanding", false);
+        }
     }
 }
